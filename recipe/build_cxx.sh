436121eaef016add59e9ae5eb1d055342d6f41f7
Silvio Traversaro <silvio@traversaro.it>
2024-10-02 11:15:33 +0200

    Add support for yarp_idl_msgs while doing cross-compilation

--- recipe/build_cxx.sh
+++ recipe/build_cxx.sh
@@ -4,6 +4,10 @@ rm -rf build
 mkdir build
 cd build
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DYARPIDL_thrift_LOCATION=$BUILD_PREFIX/bin/yarpidl_thrift -DYARPIDL_rosmsg_LOCATION=$BUILD_PREFIX/bin/yarpidl_rosmsg"
+fi
+
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS:BOOL=ON \
